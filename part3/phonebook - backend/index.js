4e56fe3c51468198f948c171166c328d36811aef
Jamie <94196481+JamieHurl@users.noreply.github.com>
2024-03-05 17:34:06 +0000

    Add files via upload

--- part3/phonebook - backend/index.js
+++ part3/phonebook - backend/index.js
@@ -1,54 +1,60 @@
-require('dotenv').config()
-const express = require('express')
-var morgan = require('morgan')
-const cors = require('cors')
+require("dotenv").config()
+const express = require("express")
+var morgan = require("morgan")
+const cors = require("cors")
 const app = express()
-const Person = require('./models/person')
+const Person = require("./models/person")
 
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('dist'))
+app.use(express.static("dist"))
 
-morgan.token('body', (request, response) => JSON.stringify(request.body));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+morgan.token("body", (request) => JSON.stringify(request.body))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>")
 })
 
-app.get('/info', (request, response) => {
-  const personCount = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  //console.log(Date())
-  response.send(`
-    <p>Phonebook has info for ${personCount} people</p>
-    <p>${Date()}</p>
-    `)
+app.get("/info", (request, response) => {
+
+  const persons = Person.find({})
+    .then((persons) => {
+      persons
+      console.log(persons)
+      const personCount = persons.length > 0
+        ? persons.length
+        : 0
+      response.send(`
+        <p>Phonebook has info for ${personCount} people</p>
+        <p>${Date()}</p>
+        `)
+    })
+  
 })
 
-app.get('/api/persons', (request, response) => {
+app.get("/api/persons", (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
   })
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person.findById(request.params.id)
-  .then(person => {
-    if (person) {
-    response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-  .catch(error => next(error))
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(response => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -58,7 +64,7 @@ const generateId = () => {
   return Math.floor(Math.random()*25000)
 }
 
-app.post('/api/persons', (request, response, next) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body
 
   const person = new Person({
@@ -68,35 +74,35 @@ app.post('/api/persons', (request, response, next) => {
   })
 
   person.save()
-  .then(savedPerson => {
-    response.json(savedPerson)
-  })
-  .catch(error => next(error))
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.put('/api/persons/:id', (request, response, next) => {
+app.put("/api/persons/:id", (request, response, next) => {
   const { name, number } = request.body
   console.log("here")
   Person
-    .findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: 'query' })
+    .findByIdAndUpdate(request.params.id, { name, number }, { new: true, runValidators: true, context: "query" })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
-    .catch(error => 
-      {
-        console.log("test")
-        next(error)
-      })
+    .catch(error =>
+    {
+      console.log("test")
+      next(error)
+    })
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.error("Heeeeeey")
+  //console.error("Heeeeeey")
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: `No record found for ID` })
-  } else if (error.name === 'ValidationError') {
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "No record found for ID" })
+  } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message })
-  } else if (error.name === 'TypeError') {
+  } else if (error.name === "TypeError") {
     return response.status(400).json({ error: error.message })
   }
   next(error)
